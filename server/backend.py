71b1b52a80528cede3d6d372223a6d68a258c7d0
Eugene Ivanchenko <me@eiva.info>
2019-02-03 21:48:48 -0500

    Initial

---
+++ server/backend.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, jsonify
+from requests import get
+
+
+app = Flask(__name__,
+            static_folder = "./dist/static",
+            template_folder = "./dist")
+
+
+@app.route('/api/<region>/<name>/id')
+def random_number(region, name):
+    return jsonify({"test": name})
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return render_template("index.html")
+
+# FLASK_APP=backend.py FLASK_DEBUG=1 flask run --host=0.0.0.0 --port=5000
